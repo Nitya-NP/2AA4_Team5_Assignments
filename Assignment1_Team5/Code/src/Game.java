2483e32429edb7cd619ebc94b67d2e84d11b6579
Raadhikka Gupta <radhika16th@gmail.com>
2026-02-07 14:36:02 -0500

    the game class

--- Assignment1_Team5/Code/src/Game.java
+++ Assignment1_Team5/Code/src/Game.java
@@ -5,43 +5,75 @@
 
 /************************************************************/
 /**
+ * This class contains the Game class that controlls the overall flow of the Catan Game. It manages the player list, tracks the number
+ * of rounds played, runs the game rounds, and ends the game when a player wins. 
  * 
+ * @author Raadhikka Gupta
  */
+
 public class Game {
 	/**
-	 * 
+	 * The number of rounds that have been played so far.
 	 */
 	private int roundsPlayed;
+
 	/**
-	 * 
+	 * The max number of rounds allowed in a game.
 	 */
 	private int maxRounds;
+
 	/**
-	 * 
+	 * The list of player participating in the game, contains null if less than 6.
 	 */
-	private Player[] players;
+	private Player[6] players;
+
 	/**
-	 * 
+	 * The board for the game.
 	 */
 	private Board board;
 
 	/**
+	 * Constructs a new game with the players and board.
 	 * 
+	 * @param players the players in the game
+	 * @param board   the board on which the game will be played
+	 */
+	public Game(Player[] players, Board board) {
+		this.roundsPlayed = 0;
+		this.maxRounds = 20;
+		this.players = players;
+		this.board = board;
+	}
+
+	/**
+	 * Starts the game and runs till a player wins or max number of rounds is reached
 	 */
 	public void start() {
+		boolean gameOver = false; // to check if game over
+
+		// Continue to play rounds till the game ends
+		while (!gameOver && roundsPlayed < maxRounds) {
+			playRound();
+			roundsPlayed++;
+
+			// Check if any player (not null) has reached the score to win
+			for (Player p : players) {
+				if (p != null && p.getPoints() >= 10) {
+					gameOver = true;
+					break;
+				}
+			}
+		}
 	}
 
 	/**
-	 * 
+	 * Plays a single round of the game
 	 */
 	public void playRound() {
-	}
-
-	/**
-	 * 
-	 * @param players 
-	 * @param board 
-	 */
-	public void Game(Player players, Board board) {
+		for (Player p : players) {
+			if (p != null) {
+				p.takeTurn();
+			}
+		}
 	}
 }
