9aff76dc21e0fa1114a381166cc41dd077dccc52
Ranica10 <ranicachawla101@gmail.com>
2026-02-13 10:49:41 -0500

    Create mechanism to roll a dice (#11)

---
+++ Assignment1_Team5/Code/src/RegularDice.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+/**
+ * A RegularDice class is able to roll a six sided die.
+ */
+public class RegularDice implements Dice {
+    /**
+     * Simulates rolling a regular six sided die.
+     * @return a random integer between 1 and 6
+     */
+    @Override
+    public int roll() {
+        Random rand = new Random();
+         // Generates a random number between 1 and 6 to return
+        return rand.nextInt(6) + 1;
+    }
+}
