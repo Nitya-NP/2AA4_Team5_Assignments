8dfa495a6535b3ccdfe69bcb7499e32e39dcbfea
Nitya Patel <117617150+Nitya-NP@users.noreply.github.com>
2026-02-13 18:42:14 -0500

    Update Player.java

--- Assignment1_Team5/Code/src/Player.java
+++ Assignment1_Team5/Code/src/Player.java
@@ -1,134 +1,120 @@
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
-
 
 /**
  * The Player classs represents an player participating in Catan simulation.
- * A Player owns resources, buildings, and winning point. 
+ * A Player owns resources, buildings, and winning points, and can take a turn.
+ * It track resources, track buildings owned, track points and exceute a random
+ * action during a turn
  * 
  * @author Nitya Patel
  */
 public class Player {
-	
-	private int playerId; //Unique identifier of the player
-	private int points; //Current winning points
-	private Map<Resources, Integer> resources; //Stores the number of resources player owns
-	private Map<Class<? extends Building>, Integer> buildings; //Stores buildings owned by the player (Roads,Settlement, Cities)
-	
+
+	private int playerId; // Unique identifier of the player
+	private int points; // Current points
+	private Map<Resources, Integer> resources; // Stores the number of resources player owns
+	private Map<Class<? extends Building>, Integer> buildings; // Stores buildings owned by the player (Roads,Settlement, Cities)
+
 	/**
 	 * Constructs a new Player with empty resources and buildings
 	 * 
 	 * @param playerId the player's unique ID
 	 */
 	public Player(int playerId) {
-		this.playerId=playerId;
-		this.points=0;
-		this.resources=new HashMap<>();
-		this.buildings=new HashMap<>();
+		this.playerId = playerId;
+		this.points = 0;
+		this.resources = new HashMap<>();
+		this.buildings = new HashMap<>();
 		initResources();
 		initBuildings();
 	}
-	
+
 	/**
-	 * Initializes all resources to 0
+	 * Initializes all resources to 0 at the start
 	 */
 	private void initResources() {
-		for(Resources r: Resources.values()) {
+		for (Resources r : Resources.values()) {
 			this.resources.put(r, 0);
 		}
 	}
-	
+
 	/**
-	 * Initializes building counts to 0
+	 * Initializes building (Road, City, Settlement) counts to 0 at the start
 	 */
 	private void initBuildings() {
-		buildings.put(Roads.class,0); 
-		buildings.put(Settlement.class,0);
-		buildings.put(Cities.class,0);
+		buildings.put(Roads.class, 0);
+		buildings.put(Settlement.class, 0);
+		buildings.put(Cities.class, 0);
 
 	}
-	
+
 	/**
 	 * @return the player's id
 	 */
 	public int getPlayerId() {
-		return this.playerId ;
+		return this.playerId;
 	}
-	
+
 	/**
-	 *  @return the player's winning points
+	 * @return the player's points
 	 */
 	public int getPoints() {
 		return this.points;
 	}
-	
+
 	/**
 	 * Adds points to the player
+	 * 
+	 * @param points number of points to add
 	 */
 	public void addPoints(int points) {
-		this.points+=points;
+		this.points += points;
 	}
-	
+
+
+
 	/**
-	 *  @return the amount of specific resource
+	 * @return total number of resources the player owns
 	 */
-	public int getResource(Resources resource) {
-		return resources.getOrDefault(resource,0);
-	}
-	
-	/**
-	 * Adds resources to player
-	 */
-	public void addResource(Resources res, int amount) {
-		resources.put(res, resources.getOrDefault(res, 0)+amount);
-	}
-	
-	/**
-	 * Removes resources if possible
-	 *  @return either true or false 
-	 */
-	public boolean removeResource(Resources res, int amount) {
-		int cur= resources.getOrDefault(res, 0);
-		if(cur>=amount) {
-			resources.put(res, cur-amount);
-			return true;
+	public int getTotalResources() {
+		int total = 0;
+		for (Integer amount : resources.values()) {
+			total += amount;
 		}
-		return false;
+		return total;
 	}
+
+	/**
+	 * Adds specific resources to the player
+	 * 
+	 * @param resource new resource to add
+	 * @param amount   the number of units to add
+	 */
+	public void addResource(Resources resource, int amount) {
+		resources.put(resource, resources.getOrDefault(resource, 0) + amount);
+	}
+
 
 	/**
 	 * Adds a building to the player based on its class name
 	 * Also updates points where applicable
+	 * 
+	 * @param building
 	 */
-	public void addBuilding(Building buil) {
-		Class<? extends Building> type = buil.getClass();
-		buildings.put(type,buildings.getOrDefault(type,0)+1);
-		
-		//Add points according to rules
-		if(buil instanceof Settlement) {
+	public void addBuilding(Building building) {
+		Class<? extends Building> type = building.getClass();
+		buildings.put(type, buildings.getOrDefault(type, 0) + 1);
+
+		// Add points according to rules
+		if (building instanceof Settlement) {
 			addPoints(1);
-		}
-		else if(buil instanceof Cities) {
+		} else if (building instanceof Cities) {
 			addPoints(2);
 		}
-		
+
 	}
-	
-	/**
-	 *  @return how many buildings of given type the player owns
-	 */
-	public int getBuilding(Class<? extends Building> type) {
-		return buildings.getOrDefault(type, 0);
-	}
-	
-	/**
-	 *  @return a summary of the player
-	 */
-	public String toString() {
-		return "Player "+ playerId + "| Points: "+ points + "| Resources: " +resources + "| Buildings: " +buildings;
-	}
+
 
 }
